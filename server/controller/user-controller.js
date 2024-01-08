1db9f3eb50d617d41a6e773af0585a8f4fd5adcc
DESKTOP-O701GTQ\Sukhi <sukhchand38@gmail.com>
2024-01-08 21:08:56 +0530

    jwt token added

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,4 +1,9 @@
 import User from "../model/user-schema.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import cookieParser from "cookie-parser";
+
+const SECRET_KEY = "secretkey"
 
 export const userSignUp = async(req, res) => {
     try {
@@ -8,7 +13,8 @@ export const userSignUp = async(req, res) => {
             return res.status(401).json({message: "Username already exist"})
         }
         const user = req.body;
-        const newUser = new User(user);
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const newUser = new User({...user, password: hashedPassword});
         await newUser.save();
         res.status(200).json({message: user})
     } catch (error) {
@@ -22,12 +28,19 @@ export const userLogin = async(req, res) => {
         const username = req.body.username;
         const password = req.body.password;
 
-        let user  = await User.findOne({username: username, password: password});
-        if(user) {
-            return res.status(200).json({user})
-        } else {
-            return res.status(401).json('Invalid login')
+        let user  = await User.findOne({username: username});
+        if(!user) {
+            return res.status(401).json('Invalid credentials')
         }
+        const isValidPassword = bcrypt.compare(password, user.password)
+        if(!isValidPassword) {
+            return res.status(401).json("invalid credentials")
+        }
+        jwt.sign({userId: res._id}, SECRET_KEY, {expiresIn: '1hr'}, (err, token)=>{
+            if(err) throw err;
+            res.cookie('token', token).json({user})
+            return res.status(200).json({user})
+        })
 
     } catch (error) {
         res.status(500).json({message: error.message})
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 import Connection from './database/db.js';
 import DefaultData from './default.js';
 import Router from './routes/route.js'
@@ -9,6 +10,7 @@ import Router from './routes/route.js'
 const app = express();
 dotenv.config();
 app.use(cors());
+app.use(cookieParser())
 app.use(bodyParser.json({extended: true}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', Router);
